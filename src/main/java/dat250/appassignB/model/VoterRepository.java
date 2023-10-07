580e60d3a4749bf036d75408757f045ec51539fa
vercel97 <veronichaceline5@gmail.com>
2023-10-07 10:49:52 +0200

    Added code for the VoterController and the AppUserController

--- src/main/java/dat250/appassignB/model/VoterRepository.java
+++ src/main/java/dat250/appassignB/model/VoterRepository.java
@@ -1,4 +1,9 @@
 package dat250.appassignB.model;
 
-public class VoterRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoterRepository extends JpaRepository<Voter, Integer> {
 }
+
