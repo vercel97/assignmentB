4c5896c5025cf339e95ebf826592864b153504d7
vercel97 <veronichaceline5@gmail.com>
2023-10-07 11:56:46 +0200

    Added the class that starts the spring boot webapplication

---
+++ src/main/java/dat250/appassignB/model/VotingApplication.java
@@ -0,0 +1,12 @@
+package dat250.appassignB.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingApplication.class, args);
+    }
+}
