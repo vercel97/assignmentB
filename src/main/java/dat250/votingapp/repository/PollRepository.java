2114864293ea8c870e253dedd506a4eb02beb4c2
iRW <raive4603@uib.no>
2023-11-13 00:51:16 +0100

    poll repo

--- src/main/java/dat250/votingapp/repository/PollRepository.java
+++ src/main/java/dat250/votingapp/repository/PollRepository.java
@@ -1,6 +1,7 @@
 package dat250.votingapp.repository;
 
 import dat250.votingapp.model.Poll;
+import dat250.votingapp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +14,7 @@ public interface PollRepository extends JpaRepository<Poll, Integer> {
     @Query("SELECT p FROM Poll p WHERE LOWER(p.pollTitle) LIKE LOWER(CONCAT('%', :title, '%'))")
     List<Poll> findByTitleContainingIgnoreCase(@Param("title") String title);
 
-
+    @Query("SELECT q FROM Question q WHERE q.pollTitle = :poll AND q.isActive = TRUE")
+    Question getActiveQuestion(@Param("poll") Poll poll);
 
 }
